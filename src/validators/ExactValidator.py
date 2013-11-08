3d721a8fd211f78db51fe3a99354e6960f98af9e
Ujjawal Misra <ujjawal.misra@vedantu.com>
2013-11-08 13:21:05 +0530

    added logs for ExactValidator

--- src/validators/ExactValidator.py
+++ src/validators/ExactValidator.py
@@ -8,20 +8,24 @@ class ExactValidator(BaseValidator):
     
     def _validate(self, criteria, toValidate):
         if None == criteria:
+            ExactValidator._LOGGER.debug("None criteria, so returning True")
             return True
         elif None == toValidate:
+            ExactValidator._LOGGER.debug("None toValidate, so returning False")
             return False
         elif not 'name' in criteria:
+            ExactValidator._LOGGER.debug("'name' not in criteria, so returning False")
             return False
         elif not criteria['name'] in toValidate:
+            ExactValidator._LOGGER.debug("criteria['name']: " + criteria['name'] + " not in toValidate, so returning False")
             return False
         elif not 'expected' in criteria:
             ExactValidator._LOGGER.debug("no expected found in criteria, so returning True")
             return True
         else:
             if type(criteria['expected']) != type(toValidate[criteria['name']]):
+                ExactValidator._LOGGER.debug("type mismatch, so returning True - type(criteria['expected']): " + str(type(criteria['expected'])) + ", type(toValidate['" + criteria['name'] + "']): " + str(type(toValidate[criteria['name']])))
                 return False
             else:
-                ExactValidator._LOGGER.debug("matching string")
+                ExactValidator._LOGGER.debug("matching string criteria['expected']: " + criteria['expected'] + ", toValidate['" + criteria['name'] + "']: " + toValidate[criteria['name']])
                 return criteria['expected'] == toValidate[criteria['name']]
-            
